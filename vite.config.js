14c4817f0d1c2b4a1ce32dd70d89d734af97cb7d
lkhagvacurenn <blkhagvasuren10@gmail.com>
2025-04-20 15:42:29 +0800

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './', // important for relative asset paths
+  base: './', // ensure relative path for assets
   server: {
     open: true,
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-    }
   }
 });
